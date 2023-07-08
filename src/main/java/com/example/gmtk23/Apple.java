5f3728c712a5b6ffca55433f712539c6a1875d50
Géza László Szeghy <szeghygl@gmail.com>
2023-07-08 22:30:43 +0200

    apple + movement

---
+++ src/main/java/com/example/gmtk23/Apple.java
@@ -0,0 +1,27 @@
+package com.example.gmtk23;
+
+public class Apple {
+    private int posX;
+    private int posY;
+
+    public Apple(int x, int y) {
+        posX = x;
+        posY = y;
+    }
+
+    public int getX() {
+        return posX;
+    }
+
+    public int getY() {
+        return posY;
+    }
+
+    public void setX(int x) {
+        posX = x;
+    }
+
+    public void setY(int y) {
+        posY = y;
+    }
+}
--- src/main/java/com/example/gmtk23/HelloApplication.java
+++ src/main/java/com/example/gmtk23/HelloApplication.java
@@ -1,9 +1,9 @@
 package com.example.gmtk23;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -11,15 +11,25 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Objects;
-
 public class HelloApplication extends Application {
     protected static ArrayList<ArrayList<Rectangle>> gameArea = new ArrayList<>();
 
+    public void updateGameArea(Apple apple) {
+        for (ArrayList<Rectangle> row: gameArea) {
+            for (Rectangle r: row) {
+                if (r.getX() == apple.getX() && r.getY() == apple.getY())
+                    r.setFill(Color.color(1, 0, 0));
+                else
+                    r.setFill(Color.color(0.29, 0.9, 0.2));
+            }
+        }
+    }
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+        Apple apple = new Apple(10, 10);
+
         Group group = new Group();
         GridPane gp = new GridPane();
         gp.setLayoutX(100);
@@ -33,21 +43,46 @@ public class HelloApplication extends Application {
         for (int i = 0; i < 40; i++) {
             gameArea.add(new ArrayList<>());
             for (int j = 0; j < 40; j++) {
-                Rectangle r = new Rectangle(10, 10, Color.color(0.29, 0.9, 0.2));
+                Rectangle r;
+                if (i == apple.getX() && j == apple.getY())
+                    r = new Rectangle(10, 10, Color.color(1, 0, 0));
+                else
+                    r = new Rectangle(10, 10, Color.color(0.29, 0.9, 0.2));
                 gameArea.get(i).add(r);
                 gp.add(r, i, j);
                 r.setX(i);
                 r.setY(j);
-                //Ellenfél táblájára kattintás esetén küld az ellenfélnek (a szerveren keresztül)
-                //egy üzenetet a célzott celláról.
-                r.setOnMouseClicked(e -> {
 
-                });
             }
         }
         gp.setGridLinesVisible(true);
         group.getChildren().add(gp);
         Scene scene = new Scene(group, 600, 600);
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.UP) {
+                apple.setY(apple.getY() - 1);
+            } else if (e.getCode() == KeyCode.DOWN) {
+                apple.setY(apple.getY() + 1);
+            } else if (e.getCode() == KeyCode.LEFT) {
+                apple.setX(apple.getX() - 1);
+            } if (e.getCode() == KeyCode.RIGHT) {
+                apple.setX(apple.getX() + 1);
+            }
+            if (apple.getX() < 0) {
+                apple.setX(40);
+            }
+            if (apple.getX() > 40) {
+                apple.setX(0);
+            }
+            if (apple.getY() < 0) {
+                apple.setY(40);
+            }
+            if (apple.getY() > 40) {
+                apple.setY(0);
+            }
+            System.out.println(apple.getX() + " " + apple.getY());
+            updateGameArea(apple);
+        });
         stage.setScene(scene);
         stage.show();
     }
